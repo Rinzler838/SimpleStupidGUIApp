f5f15b2adad407869b008ac85ea617e8f2d57c2c
Rinzler838 <dvil4688@740s-pl-105.local>
2015-11-09 09:55:17 -0700

    Added Layout stuff

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -3,34 +3,47 @@ package gui.view;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 import gui.controller.GUIController;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class GUIPanel extends JPanel
 {
 	private GUIController baseController;
 	private JButton firstButton;
 	private JTextField firstField;
+	private SpringLayout baseLayout;
 	
 	public GUIPanel(GUIController baseController)
 	{
 		this.baseController = baseController;
 		
+		baseLayout = new SpringLayout();
 		firstButton = new JButton("DO NOT CLICK THIS BUTTON...");
 		firstField = new JTextField("Type words here...");
+		
 		
 		setupPanel();
 		setupLayout();
 		setupListeners();
 	}
-	
+	/**
+	 * Helper method to load all GUI components into the panel.
+	 */
 	private void setupPanel()
 	{
-		
+		this.setLayout(baseLayout);
+		this.add(firstButton);
+		this.add(firstField);
 	}
 	
 	private void setupLayout()
 	{
-		
+		baseLayout.putConstraint(SpringLayout.SOUTH, firstButton, -10, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, firstButton, -103, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, firstField, 10, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, firstField, 149, SpringLayout.WEST, this);
 	}
 	
 	private void setupListeners()
